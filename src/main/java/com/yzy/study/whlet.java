54ebfe569f0a33f58ece806af98e8cdbd7d67910
yzy <yezy@daishupei.com>
2022-04-25 23:45:27 +0800

    m

---
+++ src/main/java/com/yzy/study/whlet.java
@@ -0,0 +1,17 @@
+package com.yzy.study;
+
+import java.util.ArrayList;
+
+public class whlet {
+
+
+    public void changeValue(Integer i){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().maxMemory());
+        System.out.println(Runtime.getRuntime().totalMemory());
+        byte[] b=new byte[4 *1024 * 1024];
+    }
+}
